226262bc68da35033b8e10612754185967216bf6
truelightwater <truelightwater@gmail.com>
2023-03-09 14:42:35 +0900

    bankProject feature/phase2.0 : slf4j log code

--- src/main/java/bankProject/Application.java
+++ src/main/java/bankProject/Application.java
@@ -4,32 +4,13 @@ import bankProject.data.CsvParser;
 import bankProject.data.Parser;
 import bankProject.data.TsvParser;
 import bankProject.service.BankAnalyzer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 
 public class Application {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
-
-    private String logTest() {
-        String name = "Spring";
-
-        log.trace("trace log = {}", name);
-        log.debug("debug log = {} ", name);
-        log.info("info log = {}", name);
-        log.warn("warn log = {}", name);
-        log.error("error log ={}", name);
-
-        return "ok";
-    }
-
     public static void main(String[] args)  {
-
-        Application ap = new Application();
-        ap.logTest();
 
         BankAnalyzer bankAnalyzer = new BankAnalyzer();
 
--- src/main/java/bankProject/service/BankService.java
+++ src/main/java/bankProject/service/BankService.java
@@ -1,11 +1,15 @@
 package bankProject.service;
 
 import bankProject.model.BankTransaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 public class BankService {
 
     private List<BankTransaction> bankTransactions;
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     public BankService(List<BankTransaction> bankTransactions) {
         this.bankTransactions = bankTransactions;
@@ -16,9 +20,22 @@ public class BankService {
         for (BankTransaction bankTransaction : bankTransactions) {
             total += bankTransaction.getAmount();
         }
-        System.out.println(total);
+
+        log.info(String.valueOf(total));
+
 
         return total;
     }
+
+    public int IncomeAmount() {
+        int total = 0;
+        for (int i = 0; i < 10; i++) {
+
+        }
+
+        return total;
+    }
+
+
 
 }
